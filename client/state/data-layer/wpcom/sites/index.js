b9abce1ebaa9b67edd89dd9f6e5fc2abe4f535f3
oskosk <oskosk@gmail.com>
2017-05-31 19:07:53 -0300

    Introduce data-layer handlers for site-settings

--- client/state/data-layer/wpcom/sites/index.js
+++ client/state/data-layer/wpcom/sites/index.js
@@ -5,9 +5,11 @@ import { mergeHandlers } from 'state/action-watchers/utils';
 import automatedTransfer from './automated-transfer';
 import comments from './comments';
 import media from './media';
+import settings from './settings';
 
 export default mergeHandlers(
 	automatedTransfer,
 	comments,
-	media
+	media,
+	settings
 );
