30f90f76fc69701452a1aa3077da5f354354c2bd
Leif Singer <git@singer.sh>
2017-06-19 12:51:33 +0200

    fix linter errors

--- client/layout/guided-tours/tours/media-basics-tour.js
+++ client/layout/guided-tours/tours/media-basics-tour.js
@@ -10,124 +10,131 @@ import Gridicon from 'gridicons';
  * Internal dependencies
  */
 import {
-    makeTour,
-    Tour,
-    Step,
-    ButtonRow,
-    Next,
-    Quit,
-    Continue
+	makeTour,
+	Tour,
+	Step,
+	ButtonRow,
+	Next,
+	Quit,
+	Continue
 } from 'layout/guided-tours/config-elements';
 import {
-    isNewUser,
+	isNewUser,
 } from 'state/ui/guided-tours/contexts';
 import { isDesktop } from 'lib/viewport';
 
 export const MediaBasicsTour = makeTour(
-    <Tour
-        name="mediaBasicsTour"
-        version="20170321"
-        path="/media"
-            when={ and(
-            isDesktop,
-            isNewUser,
-            ) }
-        >
-        <Step
-            name="init"
-            arrow="top-left"
-            target=".media-library__upload-buttons"
-            placement="below"
-            >
-            <p>
-                { translate(
-                    "Welcome to the Media Libary!  You can add media by clicking the {{icon/}} {{strong}}Add New{{/strong}} button.", {
-                            components: { icon: <Gridicon icon="gridicons-add-image" />,
-                            strong: <strong />
-                             }
-                    }
-                ) }
-            </p>
-            <ButtonRow>
-                <Next step="drag-and-drop">{ translate( "Continue" ) }</Next>
-                <Quit>{ translate( "Quit" ) }</Quit>
-            </ButtonRow>
-        </Step>
+	<Tour
+		name="mediaBasicsTour"
+		version="20170321"
+		path="/media"
+			when={ and(
+			isDesktop,
+			isNewUser,
+			) }
+		>
+		<Step
+			name="init"
+			arrow="top-left"
+			target=".media-library__upload-buttons"
+			placement="below"
+			>
+			<p>
+				{
+					translate( 'Welcome to the Media Libary! You can add media by clicking ' +
+									'the {{icon/}} {{strong}}Add New{{/strong}} button.',
+						{
+							components: {
+								icon: <Gridicon icon="gridicons-add-image" />,
+								strong: <strong />,
+							}
+						}
+					)
+				}
+			</p>
+			<ButtonRow>
+				<Next step="drag-and-drop" />
+				<Quit />
+			</ButtonRow>
+		</Step>
 
-        <Step
-            name="drag-and-drop"
-            placement="right"
-            >
-            <p>
-                { translate( "You can also drag-and-drop new media directly into your library as shown here." ) }
-            </p>
-            <img src="https://cldup.com/AnA1V5AnoE.gif"
-            style={ { marginBottom: '10px', border: '3px solid #00AADC', borderRadius: '4px' } }
-            />
+		<Step
+			name="drag-and-drop"
+			placement="right"
+			>
+			<p>
+				{ translate( 'You can also drag-and-drop new media directly into your library as shown here.' ) }
+			</p>
+			<img
+				src="https://cldup.com/AnA1V5AnoE.gif"
+				style={ { marginBottom: '10px', border: '3px solid #00AADC', borderRadius: '4px' } }
+				/>
 
-            <ButtonRow>
-                <Next step="select-image">{ translate( "Continue" ) }</Next>
-                <Quit>{ translate( "Quit" ) }</Quit>
-            </ButtonRow>
-        </Step>
+			<ButtonRow>
+				<Next step="select-image" />
+				<Quit />
+			</ButtonRow>
+		</Step>
 
-         <Step
-            name="select-image"
-            placement="below"
-            arrow="top-left"
-            target=".media-library__list-item"
-            style={ { marginTop: '-10px' } }
-            >
+		<Step
+			name="select-image"
+			placement="below"
+			arrow="top-left"
+			target=".media-library__list-item"
+			style={ { marginTop: '-10px' } }
+			>
 
-            <Continue click step="click-to-edit" target=".media-library__list-item-figure">
-                 {translate( "Click this image in your media library to continue." ) }
-            </Continue>
-        </Step>
+			<Continue click step="click-to-edit" target=".media-library__list-item-figure">
+				{ translate( 'Click this image in your media library to continue.' ) }
+			</Continue>
+		</Step>
 
-        <Step name="click-to-edit"
-            placement="below"
-            arrow="top-left"
-            //when={ and( isImageSelected ) }
-            target=".editor-media-modal__secondary-action"
-            style={ { marginLeft: '-8px' } }
-            >
-            <Continue click step="launch-modal" target=".editor-media-modal__secondary-action">
-                 {translate( "Click the Edit button to edit your selected image." ) }
-            </Continue>
-        </Step>
+		<Step name="click-to-edit"
+			placement="below"
+			arrow="top-left"
+			//when={ and( isImageSelected ) }
+			target=".editor-media-modal__secondary-action"
+			style={ { marginLeft: '-8px' } }
+			>
+			<Continue click step="launch-modal" target=".editor-media-modal__secondary-action">
+				{ translate( 'Click the Edit button to edit your selected image.' ) }
+			</Continue>
+		</Step>
 
-        <Step name="launch-modal"
-            placement="beside"
-            arrow="right-top"
-            target=".editor-media-modal-detail__sidebar"
-            >
-            <p>{ translate( "You can edit the title, add a caption, find the media URL, and see other details." ) }
-            </p>
-            <ButtonRow>
-                <Next step="edit-image">{ translate( "Continue" ) }</Next>
-                <Quit>{ translate( "Quit" ) }</Quit>
-            </ButtonRow>
-        </Step>
+		<Step name="launch-modal"
+			placement="beside"
+			arrow="right-top"
+			target=".editor-media-modal-detail__sidebar"
+			>
+			<p>{ translate( 'You can edit the title, add a caption, find the media URL, and see other details.' ) }
+			</p>
+			<ButtonRow>
+				<Next step="edit-image" />
+				<Quit />
+			</ButtonRow>
+		</Step>
 
-        <Step name="edit-image"
-            placement="below"
-            arrow="top-left"
-            target=".editor-media-modal-detail__edit"
-            >
-            <p>
-                { translate(
-                    "You can click the {{icon/}} {{strong}}Edit Image{{/strong}} button to crop, rotate, and perform basic edits to your images." , {
-                            components: {
-                            icon: <Gridicon icon="gridicons-pencil" /> ,
-                            strong: <strong />
-
-                        }
-                    }
-                ) }
-            </p>
-            <ButtonRow>
-                <Quit primary>{ translate( "Got it, I'm ready to explore!" ) }</Quit>
-            </ButtonRow>
-        </Step>
-    </Tour>
+		<Step name="edit-image"
+			placement="below"
+			arrow="top-left"
+			target=".editor-media-modal-detail__edit"
+			>
+			<p>
+				{
+					translate( 'You can click the {{icon/}} {{strong}}Edit Image{{/strong}} button to crop, rotate, and ' +
+									'perform basic edits to your images.',
+						{
+							components: {
+								icon: <Gridicon icon="gridicons-pencil" />,
+								strong: <strong />,
+							}
+						}
+					)
+				}
+			</p>
+			<ButtonRow>
+				<Quit primary>{ translate( "Got it, I'm ready to explore!" ) }</Quit>
+			</ButtonRow>
+		</Step>
+	</Tour>
 );
