ac6332fa31a0dfa95b08c43ba9e45b2494e3c7d4
Pierre Bertet <hello@pierre.world>
2017-07-14 12:09:34 +0100

    Add a top-posts component and state module
    This commit adds a `QueryTopPosts` component and a `top-posts` state
    module that can be used together to fetch the top posts by view for a
    period of time.
    
    See the REST API reference page for more information:
    https://developer.wordpress.com/docs/api/1.1/get/sites/%24site/stats/top-posts/

--- client/state/stats/reducer.js
+++ client/state/stats/reducer.js
@@ -2,10 +2,12 @@
  * Internal dependencies
  */
 import posts from './posts/reducer';
-import { combineReducers } from 'state/utils';
 import lists from './lists/reducer';
+import topPosts from './top-posts/reducer';
+import { combineReducers } from 'state/utils';
 
 export default combineReducers( {
+	topPosts,
 	posts,
-	lists
+	lists,
 } );
